080cf7b684f8666ad81df39de12283b8e6668c73
Marko Balogh <marko_balogh@me.com>
2022-11-28 15:02:14 -0800

    set up fonts / typography

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { fonts } from './typography';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Nunito-Sans-ExtraBold': require('./assets/fonts/Nunito_Sans/NunitoSans-ExtraBold.ttf'),
+    'Montserrat': require('./assets/fonts/Montserrat/static/Montserrat-Regular.ttf'),
+    'Comic-Neue': require('./assets/fonts/Comic_Neue/ComicNeue-Regular.ttf')
+  });
+
   return (
     <NavigationContainer>
       <View style={styles.container}>
-        <Text>Welcome to Struggle Bus</Text>
+        <Text style={fonts.header}>Welcome to Struggle Bus</Text>
         <StatusBar style="auto" />
       </View>
     </NavigationContainer>
---
+++ typography.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const fonts = StyleSheet.create({
+  handwriting: {
+    fontFamily: 'Comic-Neue',
+    fontSize:18
+  },
+  body: {
+    fontFamily: 'Montserrat',
+    fontSize: 16,
+    lineHeight: 25,
+    letterSpacing:2
+  },
+  header: {
+    fontFamily: 'Nunito-Sans-ExtraBold',
+    fontSize:30
+  },
+  header2: {
+    fontFamily: 'Nunito-Sans-ExtraBold',
+    fontSize:20
+  }
+})
