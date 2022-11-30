7fd3cf7313efdaf672a5cc0b5458ddf9e4062809
Marko Balogh <marko_balogh@me.com>
2022-11-29 20:12:48 -0800

    uploaded some images, created color scheme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
+import {loadAsync, useFonts } from 'expo-font';
 import { fonts } from './typography';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Home';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useState } from 'react';
 
+
+//Setup
+//Create navigator
+const Stack = createNativeStackNavigator();
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,21 +22,21 @@ export default function App() {
     'Comic-Neue': require('./assets/fonts/Comic_Neue/ComicNeue-Regular.ttf')
   });
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <Text style={fonts.header}>Welcome to Struggle Bus</Text>
-        <StatusBar style="auto" />
-      </View>
+    <NavigationContainer onReady={onLayoutRootView}>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={Home} options={{headerShown:false}}></Stack.Screen>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ theme.tsx
@@ -0,0 +1,9 @@
+export const theme = {
+  action: '#54A894',
+  greenLight: '#84DB82',
+  yellowLight: '#FFD976',
+  redLight: '#FC4949',
+  background0: '#FFFFFF',
+  background1: '#F6F6F6',
+  background2: '#DCEDEB'
+}
