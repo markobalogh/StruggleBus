60cf6b3f9f26c3a314d584c607ae70998832682d
abena19 <ofosuabena19@gmail.com>
2022-11-30 15:40:06 -0800

    Add kudos and mood buttons

---
+++ components/reusable/ActionButtonHistory.tsx
@@ -0,0 +1,36 @@
+import { StyleProp, StyleSheet, Text, TextStyle, TouchableOpacity } from "react-native";
+import { theme } from "../../theme";
+import { fonts } from "../../typography";
+
+interface ActionButtonHistoryProps {
+  onPress: () => void,
+  title: string,
+  style?:StyleProp<TextStyle>
+}
+
+export default function ActionButtonHistory({onPress,title,style}:ActionButtonHistoryProps) {
+  return (
+    <TouchableOpacity onPress={onPress} style={[style,styles.topLevel]}>
+      <Text style={[fonts.header3, styles.title]}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  topLevel: {
+    height: 40,
+    width:180,
+    maxHeight: 40,
+    maxWidth:180,
+    borderRadius: 10,
+    backgroundColor: theme.colors.action,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // padding:theme.padding0,
+  },
+  title: {
+    color:'white',
+    textAlign:'center'
+  }
+})
--- components/screens/History.tsx
+++ components/screens/History.tsx
@@ -1,15 +1,23 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Props } from "../../App";
 import { theme } from "../../theme";
 import { fonts } from "../../typography";
 import StruggleBusHeader from "../reusable/StruggleBusHeader";
+import ActionButtonHistory from "../reusable/ActionButtonHistory";
+import ActionButton from "../reusable/ActionButton";
 
 export default function History({ navigation }:Props) {
   return (
     <SafeAreaView style={styles.topLevel}>
       <StruggleBusHeader></StruggleBusHeader>
       <Text style={fonts.handwriting}>Abena, This is where you put your code!!!</Text>
+      <View style={styles.kudosMoodContainer}>
+        <ActionButtonHistory onPress={()=>{}} title="Kudos"></ActionButtonHistory>
+        <ActionButtonHistory onPress={()=>{}} title="Mood"></ActionButtonHistory> 
+      </View>
+      
+      {/* <ActionButton onPress={()=>{}} title="Check In"></ActionButton> */}
     </SafeAreaView>
   )
 }
@@ -21,5 +29,9 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'stretch',
     backgroundColor:theme.colors.background0
+  },
+  kudosMoodContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 });
